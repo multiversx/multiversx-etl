0b6d4a4a392642fe9e16503aeaea9db76c70d826
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-05-25 11:36:28 +0300

    Sketch schema transformer (refactoring needed).

--- multiversxetl/cli/__init__.py
+++ multiversxetl/cli/__init__.py
@@ -5,6 +5,7 @@ import click
 from multiversxetl.cli.plan_tasks import (inspect_tasks,
                                           plan_tasks_with_intervals,
                                           plan_tasks_without_intervals)
+from multiversxetl.cli.schema import generate_schema
 from multiversxetl.cli.work import (do_extract_with_intervals,
                                     do_extract_without_intervals,
                                     do_load_with_intervals,
@@ -25,3 +26,4 @@ cli.add_command(do_extract_with_intervals, "do_extract_with_intervals")
 cli.add_command(do_extract_without_intervals, "do_extract_without_intervals")
 cli.add_command(do_load_with_intervals, "do_load_with_intervals")
 cli.add_command(do_load_without_intervals, "do_load_without_intervals")
+cli.add_command(generate_schema, "generate_schema")
