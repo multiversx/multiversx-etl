29eb1e61f76744481a204365f04324fa90896813
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-05-30 12:22:17 +0300

    do_continuously().

--- multiversxetl/cli/work.py
+++ multiversxetl/cli/work.py
@@ -1,7 +1,8 @@
 import logging
 import socket
+import time
 from pathlib import Path
-from typing import Optional
+from typing import Callable, Optional
 
 import click
 
@@ -17,9 +18,23 @@ logging.basicConfig(level=logging.INFO)
 @click.option("--gcp-project-id", required=True, type=str, help="The GCP project ID.")
 @click.option("--worker-id", type=str, help="Worker ID (e.g. computer name).")
 @click.option("--index-name", type=str, help="Filter by index name.")
-def do_extract_with_intervals(workspace: str, gcp_project_id: str, worker_id: str, index_name: Optional[str]):
+@click.option("--continue-on-error", type=bool, help="Continue (with other tasks) on error.")
+@click.option("--sleep-between-tasks", type=int, default=5, help="Time to sleep between tasks (in seconds).")
+def do_extract_with_intervals(
+        workspace: str,
+        gcp_project_id: str,
+        worker_id: str,
+        index_name: Optional[str],
+        continue_on_error: bool,
+        sleep_between_tasks: int
+):
     storage = TasksWithIntervalStorage(gcp_project_id)
-    do_any_extract_task(workspace, storage, worker_id, index_name)
+
+    do_continuously(
+        lambda: do_any_extract_task(workspace, storage, worker_id, index_name),
+        continue_on_error,
+        sleep_between_tasks
+    )
 
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
@@ -27,12 +42,31 @@ def do_extract_with_intervals(workspace: str, gcp_project_id: str, worker_id: st
 @click.option("--gcp-project-id", required=True, type=str, help="The GCP project ID.")
 @click.option("--worker-id", type=str, help="Worker ID (e.g. computer name).")
 @click.option("--index-name", type=str, help="Filter by index name.")
-def do_extract_without_intervals(workspace: str, gcp_project_id: str, worker_id: str, index_name: Optional[str]):
+@click.option("--continue-on-error", type=bool, help="Continue (with other tasks) on error.")
+@click.option("--sleep-between-tasks", type=int, default=5, help="Time to sleep between tasks (in seconds).")
+def do_extract_without_intervals(
+        workspace: str,
+        gcp_project_id: str,
+        worker_id: str,
+        index_name: Optional[str],
+        continue_on_error: bool,
+        sleep_between_tasks: int
+):
     storage = TasksWithoutIntervalStorage(gcp_project_id)
-    do_any_extract_task(workspace, storage, worker_id, index_name)
+
+    do_continuously(
+        lambda: do_any_extract_task(workspace, storage, worker_id, index_name),
+        continue_on_error,
+        sleep_between_tasks
+    )
 
 
-def do_any_extract_task(workspace: str, storage: TasksStorage, worker_id: str, index_name: Optional[str]):
+def do_any_extract_task(
+        workspace: str,
+        storage: TasksStorage,
+        worker_id: str,
+        index_name: Optional[str]
+):
     file_storage = FileStorage(Path(workspace))
     worker_id = worker_id or socket.gethostname()
 
@@ -49,8 +83,8 @@ def do_any_extract_task(workspace: str, storage: TasksStorage, worker_id: str, i
         extract_job.run()
         storage.update_task(task.id, lambda t: t.update_on_extraction_finished(""))
     except Exception as e:
-        logging.exception(e)
         storage.update_task(task.id, lambda t: t.update_on_extraction_failure(str(e)))
+        raise
 
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
@@ -59,9 +93,24 @@ def do_any_extract_task(workspace: str, storage: TasksStorage, worker_id: str, i
 @click.option("--worker-id", type=str, help="Worker ID (e.g. computer name).")
 @click.option("--index-name", type=str, help="Filter by index name.")
 @click.option("--schema-folder", required=True, type=str, help="Folder with schema files.")
-def do_load_with_intervals(workspace: str, gcp_project_id: str, worker_id: str, index_name: Optional[str], schema_folder: str):
+@click.option("--continue-on-error", type=bool, help="Continue (with other tasks) on error.")
+@click.option("--sleep-between-tasks", type=int, default=5, help="Time to sleep between tasks (in seconds).")
+def do_load_with_intervals(
+        workspace: str,
+        gcp_project_id: str,
+        worker_id: str,
+        index_name: Optional[str],
+        schema_folder: str,
+        continue_on_error: bool,
+        sleep_between_tasks: int
+):
     storage = TasksWithIntervalStorage(gcp_project_id)
-    do_any_load_task(workspace, gcp_project_id, storage, worker_id, index_name, schema_folder)
+
+    do_continuously(
+        lambda: do_any_load_task(workspace, gcp_project_id, storage, worker_id, index_name, schema_folder),
+        continue_on_error,
+        sleep_between_tasks
+    )
 
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
@@ -70,12 +119,34 @@ def do_load_with_intervals(workspace: str, gcp_project_id: str, worker_id: str, 
 @click.option("--worker-id", type=str, help="Worker ID (e.g. computer name).")
 @click.option("--index-name", type=str, help="Filter by index name.")
 @click.option("--schema-folder", required=True, type=str, help="Folder with schema files.")
-def do_load_without_intervals(workspace: str, gcp_project_id: str, worker_id: str, index_name: Optional[str], schema_folder: str):
+@click.option("--continue-on-error", type=bool, help="Continue (with other tasks) on error.")
+@click.option("--sleep-between-tasks", type=int, default=5, help="Time to sleep between tasks (in seconds).")
+def do_load_without_intervals(
+    workspace: str,
+    gcp_project_id: str,
+    worker_id: str,
+    index_name: Optional[str],
+    schema_folder: str,
+    continue_on_error: bool,
+    sleep_between_tasks: int
+):
     storage = TasksWithoutIntervalStorage(gcp_project_id)
-    do_any_load_task(workspace, gcp_project_id, storage, worker_id, index_name, schema_folder)
+
+    do_continuously(
+        lambda: do_any_load_task(workspace, gcp_project_id, storage, worker_id, index_name, schema_folder),
+        continue_on_error,
+        sleep_between_tasks
+    )
 
 
-def do_any_load_task(workspace: str, gcp_project_id: str, storage: TasksStorage, worker_id: str, index_name: Optional[str], schema_folder: str):
+def do_any_load_task(
+        workspace: str,
+        gcp_project_id: str,
+        storage: TasksStorage,
+        worker_id: str,
+        index_name: Optional[str],
+        schema_folder: str
+):
     file_storage = FileStorage(Path(workspace))
     worker_id = worker_id or socket.gethostname()
 
@@ -97,3 +168,16 @@ def do_any_load_task(workspace: str, gcp_project_id: str, storage: TasksStorage,
     except Exception as e:
         logging.exception(e)
         storage.update_task(task.id, lambda t: t.update_on_loading_failure(str(e)))
+        raise
+
+
+def do_continuously(callable: Callable[[], None], continue_on_error: bool, sleep_between_tasks: int):
+    while True:
+        try:
+            callable()
+        except:
+            if not continue_on_error:
+                raise
+
+        print(f"Sleeping for {sleep_between_tasks} seconds...")
+        time.sleep(sleep_between_tasks)
