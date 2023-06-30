672177251be17285d543e56bf1e79f22e631d6f5
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-06-30 16:09:39 +0300

    Allow one to plan etl tasks for specific ES indices.

--- multiversxetl/cli/plan_tasks.py
+++ multiversxetl/cli/plan_tasks.py
@@ -1,10 +1,13 @@
 import datetime
 import logging
 from pprint import pprint
+from typing import Tuple
 
 import click
 
-from multiversxetl.constants import SECONDS_IN_DAY, SECONDS_IN_MINUTE
+from multiversxetl.constants import (INDICES_WITH_INTERVALS,
+                                     INDICES_WITHOUT_INTERVALS, SECONDS_IN_DAY,
+                                     SECONDS_IN_MINUTE)
 from multiversxetl.planner import (TasksPlanner, TasksWithIntervalStorage,
                                    TasksWithoutIntervalStorage)
 from multiversxetl.planner.tasks import count_tasks_by_status
@@ -38,12 +41,13 @@ def inspect_tasks(gcp_project_id: str):
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.option("--indexer-url", type=str, help="The indexer URL (Elasticsearch instance).")
+@click.option("--indices", multiple=True, default=INDICES_WITH_INTERVALS)
 @click.option("--gcp-project-id", type=str, help="The GCP project ID.")
 @click.option("--bq-dataset", type=str, required=True, help="The BigQuery dataset (destination).")
 @click.option("--start-timestamp", type=int, help="The start timestamp (e.g. genesis time).")
 @click.option("--end-timestamp", type=int, help="The end timestamp (e.g. a recent one).")
 @click.option("--granularity", type=int, default=SECONDS_IN_DAY, help="Task granularity, in seconds.")
-def plan_tasks_with_intervals(indexer_url: str, gcp_project_id: str, bq_dataset: str, start_timestamp: int, end_timestamp: int, granularity: int):
+def plan_tasks_with_intervals(indexer_url: str, indices: Tuple[str, ...], gcp_project_id: str, bq_dataset: str, start_timestamp: int, end_timestamp: int, granularity: int):
     storage = TasksWithIntervalStorage(gcp_project_id)
     planner = TasksPlanner()
 
@@ -53,6 +57,7 @@ def plan_tasks_with_intervals(indexer_url: str, gcp_project_id: str, bq_dataset:
 
     new_tasks = planner.plan_tasks_with_intervals(
         indexer_url,
+        list(indices),
         bq_dataset,
         start_timestamp,
         end_timestamp,
@@ -64,10 +69,11 @@ def plan_tasks_with_intervals(indexer_url: str, gcp_project_id: str, bq_dataset:
 
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.option("--indexer-url", type=str, help="The indexer URL (Elasticsearch instance).")
+@click.option("--indices", multiple=True, default=INDICES_WITHOUT_INTERVALS)
 @click.option("--gcp-project-id", type=str, help="The GCP project ID.")
 @click.option("--bq-dataset", type=str, help="The BigQuery dataset (destination).")
-def plan_tasks_without_intervals(indexer_url: str, gcp_project_id: str, bq_dataset: str):
+def plan_tasks_without_intervals(indexer_url: str, indices: Tuple[str, ...], gcp_project_id: str, bq_dataset: str):
     storage = TasksWithoutIntervalStorage(gcp_project_id)
     planner = TasksPlanner()
-    new_tasks = planner.plan_tasks_without_intervals(indexer_url, bq_dataset)
+    new_tasks = planner.plan_tasks_without_intervals(indexer_url, list(indices), bq_dataset)
     storage.add_tasks(new_tasks)
--- multiversxetl/constants.py
+++ multiversxetl/constants.py
@@ -1,4 +1,4 @@
 SECONDS_IN_MINUTE = 60
 SECONDS_IN_DAY = 24 * 60 * SECONDS_IN_MINUTE
 INDICES_WITH_INTERVALS = ["accountsesdt", "tokens", "blocks", "receipts", "transactions", "miniblocks", "rounds", "accountshistory", "scresults", "accountsesdthistory", "scdeploys", "logs", "operations"]
-INDICES_WITHOUT_INTERVALS = set(["accounts", "rating", "validators", "epochinfo", "tags", "delegators"]) - set(["rating", "tags"])
+INDICES_WITHOUT_INTERVALS = list(set(["accounts", "rating", "validators", "epochinfo", "tags", "delegators"]) - set(["rating", "tags"]))
