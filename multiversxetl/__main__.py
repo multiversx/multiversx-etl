59e80f04cffeec6e52806fbd46e023b7d729c72d
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-07-10 14:33:01 +0300

    Handle grpc "Corruption detected." errors.

--- multiversxetl/__main__.py
+++ multiversxetl/__main__.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sys
 
 from multiversxetl.cli import cli
@@ -7,6 +8,9 @@ from multiversxetl.errors import UsageError
 logging.basicConfig(level=logging.INFO, format="[%(threadName)s] [%(levelname)s] [%(module)s]: %(message)s")
 
 if __name__ == "__main__":
+    # See: https://github.com/grpc/grpc/issues/28557
+    os.environ["GRPC_POLL_STRATEGY"] = "poll"
+
     try:
         cli()
     except UsageError as error:
