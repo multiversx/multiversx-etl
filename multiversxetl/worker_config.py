5c29555715f14645a879e079f78603f1b7f86034
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-09-14 16:31:32 +0300

    Redesign worker config.

--- multiversxetl/worker_config.py
+++ multiversxetl/worker_config.py
@@ -10,25 +10,17 @@ class WorkerConfig:
             bq_dataset: str,
             schema_folder: Path,
             indexer_url: str,
-            indices_with_intervals: List[str],
-            indices_without_intervals: List[str],
-            time_partition_start: int,
-            time_partition_end: int,
-            interval_size_in_seconds: int,
-            num_intervals_in_bulk: int,
-            num_threads: int
+            genesis_timestamp: int,
+            append_only_indices: 'IndicesConfig',
+            mutable_indices: 'IndicesConfig'
     ) -> None:
         self.gcp_project_id = gcp_project_id
         self.bq_dataset = bq_dataset
         self.schema_folder = schema_folder
         self.indexer_url = indexer_url
-        self.indices_with_intervals = indices_with_intervals
-        self.indices_without_intervals = indices_without_intervals
-        self.time_partition_start = time_partition_start
-        self.time_partition_end = time_partition_end
-        self.interval_size_in_seconds = interval_size_in_seconds
-        self.num_intervals_in_bulk = num_intervals_in_bulk
-        self.num_threads = num_threads
+        self.genesis_timestamp = genesis_timestamp
+        self.append_only_indices = append_only_indices
+        self.mutable_indices = mutable_indices
 
     @classmethod
     def load_from_file(cls, path: Path) -> "WorkerConfig":
@@ -42,11 +34,39 @@ class WorkerConfig:
             bq_dataset=data["bq_dataset"],
             schema_folder=Path(data["schema_folder"]),
             indexer_url=data["indexer_url"],
-            indices_with_intervals=data["indices_with_intervals"],
-            indices_without_intervals=data["indices_without_intervals"],
+            genesis_timestamp=data["genesis_timestamp"],
+            append_only_indices=IndicesConfig.load_from_dict(data["append_only_indices"]),
+            mutable_indices=IndicesConfig.load_from_dict(data["mutable_indices"])
+        )
+
+
+class IndicesConfig:
+    def __init__(
+            self,
+            indices: List[str],
+            time_partition_start: int,
+            time_partition_end: int,
+            interval_size_in_seconds: int,
+            num_intervals_in_bulk: int,
+            num_threads: int,
+            should_fail_on_counts_mismatch: bool,
+    ) -> None:
+        self.indices = indices
+        self.time_partition_start = time_partition_start
+        self.time_partition_end = time_partition_end
+        self.interval_size_in_seconds = interval_size_in_seconds
+        self.num_intervals_in_bulk = num_intervals_in_bulk
+        self.num_threads = num_threads
+        self.should_fail_on_counts_mismatch = should_fail_on_counts_mismatch
+
+    @classmethod
+    def load_from_dict(cls, data: Dict[str, Any]) -> "IndicesConfig":
+        return cls(
+            indices=data["indices"],
             time_partition_start=data["time_partition_start"],
             time_partition_end=data["time_partition_end"],
             interval_size_in_seconds=data["interval_size_in_seconds"],
             num_intervals_in_bulk=data["num_intervals_in_bulk"],
-            num_threads=data["num_threads"]
+            num_threads=data["num_threads"],
+            should_fail_on_counts_mismatch=data["should_fail_on_counts_mismatch"]
         )
