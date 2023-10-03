f28d0d81615bf361d87a7bf78d4da03e8a54badc
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-10-03 13:43:57 +0300

    Add command "re-generate schema".

--- multiversxetl/app.py
+++ multiversxetl/app.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import sys
@@ -9,6 +10,7 @@ from typing import Any, List
 from multiversxetl.app_controller import AppController
 from multiversxetl.constants import SECONDS_IN_DAY, SECONDS_IN_ONE_HOUR
 from multiversxetl.errors import UsageError
+from multiversxetl.schema import map_elastic_search_schema_to_bigquery_schema
 
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(levelname)s] [%(threadName)s] [%(module)s]: %(message)s")
 # Suppress some logging from Elasticsearch.
@@ -33,21 +35,31 @@ def main(args: List[str]) -> int:
 
 def _do_main(args: List[str]):
     parser = ArgumentParser()
-    parser.add_argument("--workspace", required=True, help="Workspace path.")
 
     subparsers = parser.add_subparsers()
 
     subparser = subparsers.add_parser("etl-append-only-indices", help="Do ETL for append-only indices (continuously).")
+    subparser.add_argument("--workspace", required=True, help="Workspace path.")
     subparser.add_argument("--sleep-between-iterations", type=int, default=SECONDS_IN_ONE_HOUR)
     subparser.set_defaults(func=_do_etl_append_only_indices)
 
     subparser = subparsers.add_parser("etl-mutable-indices", help="Do ETL for mutable indices (continuously).")
+    subparser.add_argument("--workspace", required=True, help="Workspace path.")
     subparser.add_argument("--sleep-between-iterations", type=int, default=SECONDS_IN_DAY)
     subparser.set_defaults(func=_do_etl_mutable_indices)
 
     subparser = subparsers.add_parser("rewind", help="Rewind to the latest checkpoint.")
+    subparser.add_argument("--workspace", required=True, help="Workspace path.")
     subparser.set_defaults(func=_do_rewind_to_checkpoint)
 
+    subparser = subparsers.add_parser("check-append-only-indices", help="..")
+    subparser.add_argument("--workspace", required=True, help="Workspace path.")
+    subparser.set_defaults(func=_do_check_append_only_indices)
+
+    subparser = subparsers.add_parser("regenerate-schema", help="Re-generates BQ schema files from ES schema files.")
+    subparser.add_argument("--input-folder", type=str, help="The path to 'input' schema files. E.g. 'elasticreindexer/cmd/indices-creator/config/noKibana'.")
+    subparser.add_argument("--output-folder", type=str, help="The path to 'output' schema files (generated).")
+    subparser.set_defaults(func=_do_regenerate_schema)
 
     parsed_args = parser.parse_args(args)
     parsed_args.func(parsed_args)
@@ -87,6 +99,30 @@ def _do_rewind_to_checkpoint(args: Any):
     workspace = Path(args.workspace).expanduser().resolve()
     controller = AppController(workspace)
     controller.rewind_to_checkpoint()
+
+
+def _do_regenerate_schema(args: Any):
+    input_folder = Path(args.input_folder).expanduser().resolve()
+    output_folder = Path(args.output_folder).expanduser().resolve()
+
+    input_folder_path = Path(input_folder)
+    input_files = list(input_folder_path.glob("*.json"))
+
+    print(f"Found {len(input_files)} input files in {input_folder_path}")
+
+    output_folder_path = Path(output_folder)
+    output_folder_path.mkdir(parents=True, exist_ok=True)
+
+    for input_file in input_files:
+        input_schema = json.loads(input_file.read_text())
+        output_schema = map_elastic_search_schema_to_bigquery_schema(input_schema)
+
+        if not output_schema:
+            print("No schema, skipping file:", input_file)
+            continue
+
+        output_file_path = output_folder_path / input_file.name
+        output_file_path.write_text(json.dumps(output_schema, indent=4) + "\n")
 
 
 if __name__ == "__main__":
